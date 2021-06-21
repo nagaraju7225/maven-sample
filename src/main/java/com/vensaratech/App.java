6745a8decb96eb4b9a309cad52458e36515522de
nagaraju d <nagaraju7225@gmail.com>
2021-06-21 12:14:16 +0000

    sample java project

---
+++ src/main/java/com/vensaratech/App.java
@@ -0,0 +1,19 @@
+package com.vensaratech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to Maven" );
+        naga();
+    }
+    public static void naga()
+    {
+        System.out.println( "This is naga file output" );
+    }
+}
